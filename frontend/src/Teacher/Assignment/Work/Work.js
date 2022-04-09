a59684049ee6457610a3dc26ddc225f61d402662
longtrinh1749 <trinhlong1749@gmail.com>
2022-04-09 16:06:06 +0700

    update objects to work api

--- frontend/src/Teacher/Assignment/Work/Work.js
+++ frontend/src/Teacher/Assignment/Work/Work.js
@@ -20,7 +20,8 @@ function callGetWork(credentials) {
             "sample/data/AssignmentDetail/Bùi Trung Anh/trang_0.jpg",
             "sample/data/AssignmentDetail/Bùi Trung Anh/trang_1.jpg",
             "sample/data/AssignmentDetail/Bùi Trung Anh/trang_2.jpg",
-        ]
+        ],
+        "objects": []
     }
 }
 
@@ -32,21 +33,19 @@ function drawingCanvas() {
     console.log("Canvas layer Clicked");
 }
 
-function objectClicked() {
-    console.log("Object Clicked");
-}
-
 export default function Work(props) {
-
+    
+    const work = callGetWork()
     const symbol = {
         WRONG: 'wrong',
         RIGHT: 'right',
         WRONG_IMG: "img/wrong1.png",
         RIGHT_IMG: "img/right1.png",
+        WIDTH: 40,
+        HEIGHT: 40,
     }
 
-    const [objects, setObjects] = useState([]);
-    let a =1;
+    const [objects, setObjects] = useState(work.objects);
     const [objectSpans, setObjectSpans] = useState();
     // var objectSpans = objects.map((object, index) => 
     //     <span className="object-symbol-container" style={{left:object.left, top:object.top}}>
@@ -61,10 +60,9 @@ export default function Work(props) {
     }
 
     var gradingTool = tools.SYMBOL;
-    const work = callGetWork()
     console.log("asd" + props.assignment.id);
     const listSubmitted = work.works.map((submit, index) =>
-        <img id={"work-img-" + index} index={index} src={submit} onClick={(e) => imageClicked(e, index)}/>
+        <img id={"work-img-" + index} index={index} src={submit} onClick={(e) => imageClicked(e, index)} />
     )
     function imageClicked(e, i) {
         console.log("Image layer Clicked height" + i);
@@ -73,7 +71,7 @@ export default function Work(props) {
         var x = e.clientX - rect.left; //x position within the element.
         var y = e.clientY - rect.top;  //y position within the element.
         console.log("Left? : " + x + " ; Top? : " + y + ".");
-        objects.push({left: x-30, top: y-30, image:i})
+        objects.push({ left: x - 30, top: y - 30, image: i, type: symbol.RIGHT })
         setObjects(objects)
         console.log(objects)
         setObjectSpans(objects.map((object, index) => {
@@ -81,13 +79,61 @@ export default function Work(props) {
             for (let i = 0; i < object.image; i++) {
                 imgHeight += document.getElementById('work-img-' + i).clientHeight
             }
+            let _src = ""
+            if (object.type == symbol.RIGHT) {
+                _src = symbol.RIGHT_IMG
+            } else if (object.type == symbol.WRONG) {
+                _src = symbol.WRONG_IMG
+            }
             return (
-            <span className="object-symbol-container" style={{left:object.left, top:(object.top + imgHeight)}}>
-                <img className="object-symbol object" src={symbol.RIGHT_IMG} />
-            </span>
+                <span className="object-symbol-container" onClick={(e) => objectClicked(e)} style={{ left: object.left, top: (object.top + imgHeight) }}>
+                    <img className="object-symbol object" src={_src} />
+                </span>
             )
+        }))
+    }
+
+    function objectClicked(e, l, t) {
+        console.log("Objectt Clicked" + e);
+        // var rect = e.target.getBoundingClientRect();
+        // var x = e.clientX - rect.left; //x position within the element.
+        // var y = e.clientY - rect.top;  //y position within the element.
+        var objectIndexToRemove = -1
+        console.log("mouse location ", l, t);
+        for (let i = 0; i < objects.length; i++) {
+            // if (objects[i].left > x && objects[i].left < (x + symbol.WIDTH) && objects[i].top > y && objects[i].top < (y + symbol.HEIGHT)) {
+            if (objects[i].left == l && objects[i].top == t) {
+                if (objects[i].type == symbol.RIGHT) {
+                    objects[i].type = symbol.WRONG
+                    break
+                } else if (objects[i].type == symbol.WRONG) {
+                    objectIndexToRemove = i
+                    break
+                }
+            }
         }
-        ))
+        console.log("found right symbol " + objectIndexToRemove)
+        if (objectIndexToRemove > -1) {
+            objects.splice(objectIndexToRemove, 1)
+        }
+        setObjects(objects)
+        setObjectSpans(objects.map((object, index) => {
+            var imgHeight = 0
+            for (let i = 0; i < object.image; i++) {
+                imgHeight += document.getElementById('work-img-' + i).clientHeight
+            }
+            let _src = ""
+            if (object.type == symbol.RIGHT) {
+                _src = symbol.RIGHT_IMG
+            } else if (object.type == symbol.WRONG) {
+                _src = symbol.WRONG_IMG
+            }
+            return (
+                <span className="object-symbol-container" onClick={(e) => objectClicked(e, object.left, object.top)} style={{ left: object.left, top: (object.top + imgHeight) }}>
+                    <img className="object-symbol object" src={_src} />
+                </span>
+            )
+        }))
     }
     function toolCommentClicked() {
         console.log("Tool comment Clicked" + this.gradingTool);
@@ -137,12 +183,16 @@ export default function Work(props) {
                         onClick={drawingCanvas}>
                         <canvas id="my-canvas" className="my-canvas" />
                     </div> */}
-                    <div className="object-layer layer" id="object-layer" 
-                    // style={{ height: 2301 }}
+
+                    {/* 
+                        sinh canvas tu image
+                    */}
+                    <div className="object-layer layer" id="object-layer"
+                        // style={{ height: 2301 }}
                         onClick={objectLayerClicked}>
-                        <span className="object-symbol-container">
+                        {/* <span className="object-symbol-container">
                             <img className="object-symbol object" src="img/right1.png" />
-                        </span>
+                        </span> */}
                         {objectSpans}
                         {/* <ObjectGradingLayer /> */}
                     </div>
